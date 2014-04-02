e0df22825d62b2771d2830b4b55e11ca96a4c8a0
Deeksha Chugh <cancerian.dia+github@gmail.com>
2014-04-02 16:41:59 -0700

    Changed the Layout

--- homework2/ui.R
+++ homework2/ui.R
@@ -1,50 +1,40 @@
 library(shiny)
 
-data(iris)
 
-shinyUI(
-  pageWithSidebar(
-    headerPanel("Movies Data"),
-    
-    sidebarPanel(
-      radioButtons(
-        "highlight",
-        "MPAA Rating",
-        c("All", "NC-17", "PG", "PG-13","R")
-       
-      ),
+shinyUI(fluidPage(
+  
+  headerPanel("Movie Reviews"),
+  mainPanel(plotOutput("scatterplot"), width = 10),
+  fluidRow(
+      column(2,offset=1, h5("Ratings Explorer"),
+      wellPanel(
+        radioButtons( "highlight", "", c("All", "NC-17", "PG", "PG-13","R"))
+            )
+          ),
       
-      checkboxGroupInput(
-        "GenreSelection",
-        "Movie Genres:",
+      column(2, h5("Genres Explorer"),
+        wellPanel(
+        checkboxGroupInput( "GenreSelection",
+        "",
         c("Action", "Animation", "Comedy","Drama","Documentary", "Mixed", "None", "Romance", "Short"),
         selected = c("Action", "Animation", "Comedy","Drama","Documentary", "Mixed", "None", "Romance", "Short")
-      ),
-      sliderInput("dotsize", 
-                  "Dot Size:", 
-                  min = 1,
-                  max = 10, 
-                  value = 2
-      ),
+      ))),
+      column(2, h5("Size"),
+             wellPanel(
+             sliderInput("dotsize", "",  min = 1, max = 10, value = 2)
+             )),
       
-      sliderInput("alphasize", 
-                  "Alpha Size:", 
-                  min = .1,
-                  max = 1, 
-                  value = 0.99
-      ),
+      column(2,h5("Transparency"),
+             wellPanel(
+             sliderInput("alphasize", "", min = .1,max = 1, value = 0.99)
+             )),
       
-      selectInput(
-        # This will be the variable we access later.
-        "colorScheme",
-        # This will be the control title.
-        "Color Scheme:",
-        # This will be the control choices.
-        choices = c("Default", "Accent", "Set1", "Set2", "Set3", "Dark2", "Pastel1", "Pastel2")
-      ),
+      column(2,h5("Color Scheme"),
+             wellPanel(
+             selectInput( "colorScheme", "", choices = c("Set1","Default", "Accent" , "Set2", "Set3", "Dark2", "Pastel1", "Pastel2")))),
       width = 2
-    ),
+    )
     
-    mainPanel(plotOutput("scatterplot"), width = 10)
+    
   )
 )
